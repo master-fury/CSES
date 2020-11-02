8da0f1c7b9c62eda1566ee307291a787b4572271
Manish Kumar <master20121997@gmail.com>
2020-11-02 23:09:29 +0530

    Update Werid Algorithm.cpp

--- Werid Algorithm.cpp
+++ Werid Algorithm.cpp
@@ -1,3 +1,30 @@
+/* Question ---> 
+You are given all numbers between 1,2,…,n except one. Your task is to find the missing number.
+
+Input
+
+The first input line contains an integer n.
+
+The second line contains n−1 numbers. Each number is distinct and between 1 and n (inclusive).
+
+Output
+
+Print the missing number.
+
+Constraints
+2≤n≤2⋅105
+Example
+
+Input:
+5
+2 3 1 5
+
+Output:
+4
+
+*/
+
+
 #include <bits/stdc++.h>  // This will work only for g++ compiler. 
 
 
@@ -48,7 +75,7 @@ int main() {
     cout.precision(10);
     cout << fixed;
     long long int n;
-    cin>>n;
+    cin>>n;     //input number
     cout<<n<<" ";
     while(n!=1){
     	if (n%2 == 0){
